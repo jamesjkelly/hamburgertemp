5218851a4492198ee0ed202261d5fa51c030040f
jamesjkelly <jkellynjus@hotmail.com>
2017-11-23 13:55:42 -0500

    Add files via upload

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -1,80 +1,85 @@
-// require express and the burgers model
+//  includes models dir
+// --------------------
 var db = require('../models');
 var Burger = db.Burger;
-
-// export the routes
+//---------------
+// routes exports
+//---------------
 module.exports = function(app) {
-    // get the root route
-    app.get('/', function(request, response) {
-        var object = {};
-        Burger.findAll({
-            where: {
-                devoured: false
-            }
-        }).then(function(result) {
-            object.uneatenBurgers = result;
-            return;
-        }).then(function() {
-          return Burger.findAll({
-              where: {
-                  devoured: true
-              }
-          });
-        }).then(function(result) {
-            object.eatenBurgers = result;
-            return;
-        }).then(function() {
-            response.render('index', {
-                uneatenBurgers: object.uneatenBurgers,
-                eatenBurgers: object.eatenBurgers
-            }); 
-        });
-    });
-
-    // define the get api/burgers route - for all burger data
-    app.get('/api/burgers', function(request, response) {
-        Burger.findAll({}).then(function(result) {
-            response.json(result);
-        });
-    });
-
-    // define post for creating a burger
-    app.post('/', function(request, response) {
-        var newBurger = request.body.burger;
-        // if no burger is defined just return
-        if (newBurger === '') {
-            response.redirect('/');
-            return;
+//  root route path
+app.get('/', function(request, response) {
+    var object = {};
+    Burger.findAll({
+        where: {
+            devoured: false
         }
-        // create that burger
-        Burger.create({
-            burger_name: newBurger
-        }).then(function() {
-            response.redirect('/');
-        });
+    }).then(function(result) {
+        object.uneatenBurgers = result;
+        return;
+    }).then(function() {
+      return Burger.findAll({
+          where: {
+              devoured: true
+          }
+      });
+    }).then(function(result) {
+        object.eatenBurgers = result;
+        return;
+    }).then(function() {
+        response.render('index', {
+            uneatenBurgers: object.uneatenBurgers,
+            eatenBurgers: object.eatenBurgers
+        }); 
     });
+});
 
-    // define the get api/burgers/:id route - for single burger data
-    app.get('/api/burgers/:id', function(request, response) {
-        Burger.findOne({
-            where: {
-                id: request.params.id
-            }
-        }).then(function(data) {
-            response.json(data);
-        });
+// route for burgers
+// -----------------
+app.get('/api/burgers', function(request, response) {
+    Burger.findAll({}).then(function(result) {
+        response.json(result);
     });
+});
+//  new burger 
+// -----------
+app.post('/', function(request, response) {
+    var newBurger = request.body.burger;
+    if (newBurger === '') {
+        response.redirect('/');
+        return;
+    }
+    // burger create
+    // -------------
+    Burger.create({
+        burger_name: newBurger
+    }).then(function() {
+        response.redirect('/');
+    });
+});
 
-    // define put for updating a burger
-    app.put('/:id', function(request, response) {
-        Burger.update({
-            devoured: true
-        }, {
-            where: {
-                id: request.params.id
-            }
-        }).then(function() {
-            response.redirect('/');
-        });
+// route for api
+// -------------
+app.get('/api/burgers/:id', function(request, response) {
+    Burger.findOne({
+        where: {
+            id: request.params.id
+        }
+    }).then(function(data) {
+        response.json(data);
     });
+});
+
+// burger update
+// -------------
+app.put('/:id', function(request, response) {
+    Burger.update({
+        devoured: true
+    }, {
+        where: {
+            id: request.params.id
+        }
+    }).then(function() {
+        response.redirect('/');
+    });
+});
 };
--- db/schema.sql
+++ db/schema.sql
@@ -1,2 +1,4 @@
+-- create database for the awesome burgers
+-- since sequalizer can't do the needful
 CREATE DATABASE sequelize_burgers_db;
 USE sequelize_burgers_db;
